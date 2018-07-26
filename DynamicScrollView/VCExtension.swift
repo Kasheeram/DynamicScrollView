9e3f60e6a8343859b5155325332e75eb2516a475
kasheeram <kashee.nitd2016@gmail.com>
2018-07-26 19:08:06 +0530

    initial commit

---
+++ DynamicScrollView/VCExtension.swift
@@ -0,0 +1,119 @@
+//
+//  VCExtension.swift
+//  DynamicScrollView
+//
+//  Created by kashee on 26/07/18.
+//  Copyright © 2018 kashee. All rights reserved.
+//
+
+import Foundation
+
+extension ViewController {
+    
+    func setAutolayoutConstraints(){
+        
+        view.addSubview(topView)
+        topView.addSubview(backButton)
+        view.addSubview(scrollView)
+        scrollView.addSubview(titalLabel)
+        scrollView.addSubview(postedByLabel)
+        scrollView.addSubview(profileImageView)
+        scrollView.addSubview(roleLabel)
+        scrollView.addSubview(postdOnLabel)
+        scrollView.addSubview(descriptionLabel)
+        scrollView.addSubview(descriptionTextView)
+        scrollView.addSubview(attachmentsLabel)
+        scrollView.addSubview(scrollViewImages)
+        scrollView.addSubview(pageControl)
+        scrollView.addSubview(requestMoreInfoLabel)
+        
+        topView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        backButton.topAnchor.constraint(equalTo: topView.topAnchor,constant:35).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: topView.leadingAnchor).isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        scrollView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: topView.leadingAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        titalLabel.topAnchor.constraint(equalTo: scrollView.topAnchor,constant:20).isActive = true
+        titalLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor,constant:20).isActive = true
+        titalLabel.widthAnchor.constraint(equalTo: view.widthAnchor,constant:-40).isActive = true
+        titalLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 35).isActive = true
+        //      titalLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        postedByLabel.topAnchor.constraint(equalTo: titalLabel.bottomAnchor,constant:15).isActive = true
+        postedByLabel.leadingAnchor.constraint(equalTo: titalLabel.leadingAnchor).isActive = true
+        postedByLabel.trailingAnchor.constraint(equalTo: titalLabel.trailingAnchor).isActive = true
+        postedByLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        profileImageView.topAnchor.constraint(equalTo: postedByLabel.bottomAnchor,constant:10).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: postedByLabel.leadingAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        roleLabel.topAnchor.constraint(equalTo: profileImageView.topAnchor).isActive = true
+        roleLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor,constant:10).isActive = true
+        roleLabel.trailingAnchor.constraint(equalTo: titalLabel.trailingAnchor).isActive = true
+        roleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        postdOnLabel.topAnchor.constraint(equalTo: roleLabel.bottomAnchor,constant:1).isActive = true
+        postdOnLabel.leadingAnchor.constraint(equalTo: roleLabel.leadingAnchor).isActive = true
+        postdOnLabel.trailingAnchor.constraint(equalTo: roleLabel.trailingAnchor).isActive = true
+        postdOnLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        descriptionLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor,constant:20).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: postedByLabel.leadingAnchor).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: postedByLabel.trailingAnchor).isActive = true
+        descriptionLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor,constant:1).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor).isActive = true
+        descriptionTextView.widthAnchor.constraint(equalTo: view.widthAnchor,constant:-40).isActive = true
+        descriptionTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 20).isActive = true
+        
+        attachmentsLabel.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor,constant:20).isActive = true
+        attachmentsLabel.leadingAnchor.constraint(equalTo: postedByLabel.leadingAnchor).isActive = true
+        attachmentsLabel.trailingAnchor.constraint(equalTo: postedByLabel.trailingAnchor).isActive = true
+        attachmentsLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        scrollViewImages.topAnchor.constraint(equalTo: attachmentsLabel.bottomAnchor,constant:5).isActive = true
+        scrollViewImages.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor).isActive = true
+        scrollViewImages.widthAnchor.constraint(equalTo: descriptionTextView.widthAnchor).isActive = true
+        scrollViewImages.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+//        if attachmentArray.count>0{
+//            scrollViewImages.heightAnchor.constraint(equalToConstant: 180).isActive = true
+//            attachmentsLabel.isHidden = false
+//            pageControl.isHidden = false
+//        }else{
+//            scrollViewImages.heightAnchor.constraint(equalToConstant: 1).isActive = true
+//            attachmentsLabel.isHidden = true
+//            pageControl.isHidden = true
+//        }
+//        self.scrollViewImages.layoutIfNeeded()
+        
+        pageControl.topAnchor.constraint(equalTo: scrollViewImages.bottomAnchor).isActive = true
+        pageControl.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor).isActive = true
+        pageControl.widthAnchor.constraint(equalTo: descriptionTextView.widthAnchor).isActive = true
+        pageControl.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        
+        requestMoreInfoLabel.topAnchor.constraint(equalTo: pageControl.bottomAnchor,constant:15).isActive = true
+        requestMoreInfoLabel.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor).isActive = true
+        requestMoreInfoLabel.widthAnchor.constraint(equalTo: descriptionTextView.widthAnchor).isActive = true
+        requestMoreInfoLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        if #available(iOS 11.0, *) {
+            requestMoreInfoLabel.bottomAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.bottomAnchor,constant:-20).isActive = true
+        } else {
+            requestMoreInfoLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor,constant:-20).isActive = true
+        }
+        
+    }
+}
--- DynamicScrollView/ViewController.swift
+++ DynamicScrollView/ViewController.swift
@@ -8,11 +8,179 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
 
+    
+    var images:[String] = ["announcement","complaints","leave","polls"]
+    var frame = CGRect(x:0,y:0,width:0,height:0)
+    var isAttachment:Bool?
+    
+    let topView:UIView = {
+        let view =  UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .orange
+        return view
+    }()
+    
+    let backButton:UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "close_avatar"), for: .normal)
+        
+//        button.addTarget(self, action: #selector(ViewPostViewController.closeThisView), for: .touchUpInside)
+        return button
+    }()
+    
+    let scrollView:UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    
+    let titalLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Announcement Tital Low Attendance Hostel Block D"
+        label.numberOfLines = 0
+        label.sizeToFit()
+//        label.font = UIFont.sfDisplayBold(ofSize: 20)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 1)
+        return label
+    }()
+    
+    let postedByLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Posted by"
+//        label.font = UIFont.sfDisplayRegular(ofSize: 16)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 0.75)
+        return label
+    }()
+    
+    let profileImageView:UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "announcement"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 25
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let roleLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Admin"
+//        label.font = UIFont.sfDisplayBold(ofSize: 16)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 1)
+        return label
+    }()
+    
+    let postdOnLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "at2:06pm, 13th jun 2018"
+//        label.font = UIFont.sfDisplayRegular(ofSize: 14)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 1)
+        return label
+    }()
+    
+    let descriptionLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Description"
+//        label.font = UIFont.sfDisplayRegular(ofSize: 16)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 0.75)
+        return label
+    }()
+    
+    let descriptionTextView:UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "My eye will be operated and will be admitted in the hospital for 4 days. My eye will be operated and will be admitted in the hospital for 4 days."
+        textView.isScrollEnabled = false
+        textView.isUserInteractionEnabled = false
+//        textView.font = UIFont.sfDisplayRegular(ofSize: 16)
+        textView.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 0.75)
+        return textView
+    }()
+    
+    let attachmentsLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Attachments"
+//        label.font = UIFont.sfDisplayRegular(ofSize: 16)
+        label.textColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 0.75)
+        return label
+    }()
+    
+    let scrollViewImages:UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scroll
+    }()
+    
+    let attachmentImageView:UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "polls"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    lazy var pageControl:UIPageControl = {
+        let pc = UIPageControl()
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        pc.currentPage = 0
+        pc.currentPageIndicatorTintColor = UIColor(red: 33.0/255.0, green: 73.0/255.0, blue: 88.0/255.0, alpha: 1)
+        pc.pageIndicatorTintColor = UIColor(red: 197.0/255, green: 202.0/255, blue: 204.0/255, alpha: 1)
+        return pc
+    }()
+    
+    let requestMoreInfoLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Request More Info?"
+//        label.font = UIFont.sfDisplaySemibold(ofSize: 16)
+        label.textColor = UIColor(red: 0.0/255.0, green: 183.0/255.0, blue: 224.0/255.0, alpha: 1)
+        return label
+    }()
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        pageControl.numberOfPages = images.count
+        
+        if images.count > 0{
+            for index in 0..<images.count{
+                frame.origin.x = scrollViewImages.frame.size.width * CGFloat(index)
+                frame.size = scrollViewImages.frame.size
+                let imageView = UIImageView(frame: frame)
+                imageView.tag = index
+                imageView.image = UIImage(named: images[index])
+                scrollViewImages.addSubview(imageView)
+                
+            }
+            scrollViewImages.contentSize = CGSize(width:(scrollViewImages.frame.size.width * CGFloat(images.count)), height:scrollViewImages.frame.size.height)
+            
+            self.scrollViewImages.isHidden = false
+            self.view.layoutIfNeeded()
+            
+            scrollViewImages.delegate = self
+            scrollViewImages.isPagingEnabled = true
+            scrollViewImages.showsHorizontalScrollIndicator = false
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.backgroundColor = .white
+        setAutolayoutConstraints()
     }
 
     override func didReceiveMemoryWarning() {
